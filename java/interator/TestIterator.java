35b818718b988b6fd66781e09dc2be6799da4277
Zuki Desu <84729028+zukigit@users.noreply.github.com>
2022-08-16 19:12:55 +0630

    Update TestIterator.java

--- java/interator/TestIterator.java
+++ java/interator/TestIterator.java
@@ -17,7 +17,8 @@ class TestIterator {
 		while(i.hasNext()) {
 			// i.next();
 			if(count % 2 == 0) {
-				// i.remove();
+				// i.remove();  //next() method က collection ထဲမှာရှိတဲ့ element ကိုပါဖျက်ပေးတာတွေ့ရပါတယ်ဗျ။
+						//so why do we need remove() moethods ser, i read doc but didn't understand clearly ser! pls tell me, why do we still need remove() method.
 				i.next();
 			}
 			count++;
